d44018fb21adba9ac01ad4ede3d834f5b22efb50
mishateml <misha.teml@gmail.com>
2022-03-19 22:34:05 +0200

    add MultiBot fetchers

--- MultBotServ.py
+++ MultBotServ.py
@@ -1,4 +1,8 @@
+
+#!/usr/bin/env python3
+import base64
 import socketserver
+import requests
 
 class BotHandler(socketserver.BaseRequestHandler):
 
@@ -11,6 +15,16 @@ class BotHandler(socketserver.BaseRequestHandler):
            self.request.sendall(self.data.upper())
 
 HOST, PORT = "", 8000
+
+url = 'https://raw.githubusercontent.com/mishateml/hackBook/master/commands.sh'
+req = requests.get(url)
+if req.status_code == requests.codes.ok:
+    req = req.json()  # the response is a JSON
+    # req is now a dict with keys: name, encoding, url, size ...
+    # and content. But it is encoded with base64.
+    content = base64.decodestring(req['content'])
+else:
+    print('Content was not found.')
 
 tcpServer = socketserver.TCPServer((HOST, PORT), BotHandler)
 
